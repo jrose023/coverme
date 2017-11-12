0cb435caeaa30947cc3042a67f0686a1f13a93b9
nrg294 <nrg294@nyu.edu>
2017-11-12 15:04:48 -0500

    initial project
    empty project file

---
+++ CoverMe/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoverMe
+//
+//  Created by Nathan Grimshaw on 11/12/17.
+//  Copyright © 2017 nyu.edu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
