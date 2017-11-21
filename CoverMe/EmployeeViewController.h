14654b7e8e1eac0870db6736a0210b0d23ea002f
jrose023 <jar892@nyu.edu>
2017-11-21 17:33:21 -0500

    more storyboard updates

---
+++ CoverMe/EmployeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmployeeViewController.h
+//  CoverMe
+//
+//  Created by Julie Rosenbaum on 11/21/17.
+//  Copyright © 2017 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmployeeViewController : UIViewController
+
+@end
---
+++ CoverMe/EmployeeViewController.m
@@ -0,0 +1,37 @@
+//
+//  EmployeeViewController.m
+//  CoverMe
+//
+//  Created by Julie Rosenbaum on 11/21/17.
+//  Copyright © 2017 nyu.edu. All rights reserved.
+//
+
+#import "EmployeeViewController.h"
+
+@interface EmployeeViewController ()
+
+@end
+
+@implementation EmployeeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
