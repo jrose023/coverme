e0b405ae0d1b2754ee9514161bec92986c3e4fb8
jrose023 <jar892@nyu.edu>
2017-11-28 22:54:11 -0500

    storyboard updates

---
+++ CoverMe/OverViewController.h
@@ -0,0 +1,13 @@
+//
+//  OverViewController.h
+//  CoverMe
+//
+//  Created by Julie Rosenbaum on 11/22/17.
+//  Copyright © 2017 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OverViewController : UIViewController
+
+@end
