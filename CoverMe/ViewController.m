833f299a869ed87749a4c7960daf4400eb9eaedc
nrg294 <nrg294@nyu.edu>
2017-12-04 12:51:06 -0500

    firestore is now being used to fill out username and password

--- CoverMe/ViewController.m
+++ CoverMe/ViewController.m
@@ -7,14 +7,18 @@
 //
 
 #import "ViewController.h"
+@import Firebase;
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *email;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+@property (strong, nonatomic) FIRDatabaseReference *ref;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -24,6 +28,39 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void) setEmail:(UITextField *)email
+{
+    _ref = [[FIRDatabase database] reference];
+    //email.text = [NSString stringWithFormat:@"hi"];
+    [[[_ref child:@"users"] child:@"nrg294"] observeSingleEventOfType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        NSLog(@"hi");
+        // Get user value
+        NSString *user= snapshot.value[@"username"];
+        email.text = [NSString stringWithFormat:@"%@", user];
+        
+        // ...
+    } withCancelBlock:^(NSError * _Nonnull error) {
+        NSLog(@"%@", error.localizedDescription);
+    }];
+}
+
+-(void) setPassword:(UITextField *)password
+{
+    _ref = [[FIRDatabase database] reference];
+    //email.text = [NSString stringWithFormat:@"hi"];
+    [[[_ref child:@"users"] child:@"nrg294"] observeSingleEventOfType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        NSLog(@"hi");
+        // Get user value
+        NSString *user= snapshot.value[@"password"];
+        password.text = [NSString stringWithFormat:@"%@", user];
+        
+        // ...
+    } withCancelBlock:^(NSError * _Nonnull error) {
+        NSLog(@"%@", error.localizedDescription);
+    }];
+}
+
 
 
 @end
